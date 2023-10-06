3e4865f23c01cccb91f6d2d9c8c12c577db46016
KongoBongo7809 <iambornildas@gmail.com>
2023-10-06 17:59:27 -0400

    commit v1.0.1
    Author: Bornil Das
    Date Oct 6, 2023
    Description: Basis for the character controller. Added a few obstacles as well.

---
+++ TSA 23-24 Video Game Project/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
