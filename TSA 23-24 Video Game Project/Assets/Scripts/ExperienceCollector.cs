14d026a51fc4c267a909172fec3f4e69ea44b73c
KongoBongo7809 <iambornildas@gmail.com>
2024-01-24 22:06:15 -0500

    commit v2.0.2
    Author: Bornil Das
    Date: Jan 24 2024
    Description: Added experience points, counter, and enemy animations.

---
+++ TSA 23-24 Video Game Project/Assets/Scripts/ExperienceCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExperienceCollector : MonoBehaviour
+{
+    private int experience = 0;
+
+    [SerializeField] private Text experienceText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Experience"))
+        {
+            Destroy(collision.gameObject);
+            experience++;
+            experienceText.text = "EXP: " + experience;
+        }
+    }
+}
